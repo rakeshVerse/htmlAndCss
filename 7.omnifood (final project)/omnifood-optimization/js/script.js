675b2284e466fe4b8bc91ad7afccbe947a51fd45
fab-rakesh <ed.rakesh.ramteke@gmail.com>
2023-09-03 18:46:38 +0530

    Smooth scroll

--- 7.omnifood (final project)/omnifood-optimization/js/script.js
+++ 7.omnifood (final project)/omnifood-optimization/js/script.js
@@ -13,6 +13,32 @@ const yearEl = document.querySelector(".year");
 yearEl.textContent = new Date().getFullYear();
 
 ///////////////////////////////////////////////////////////
+
+// Smooth scrolling
+
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#") window.scrollTo({ top: 0, behaviour: "smooth" });
+
+    // Scroll to sections
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+///////////////////////////////////////////////////////////
+
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
